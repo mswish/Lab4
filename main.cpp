50c4d70d658fafadae3d9b1197ed4c837aebce80
swisher <swisher@lab2-3.eng.utah.edu>
2016-09-14 12:44:45 -0600

    travis take 2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
 int main(int argc, char ** argv)
 {
-    std:: cout << "Hello World This is Swish, We are uhhh here in uhhhh L130 and uhhh yeaaaaaaaaaaaaaaaaah" std::endl;
+    std:: cout << "Hello World This is Swish, We are uhhh here in uhhhh L130 and uhhh yeaaaaaaaaaaaaaaaaah" << std::endl;
 }
